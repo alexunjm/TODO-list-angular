982e96f8fbff81a67126d598edfe1e183b207073
alexanderjaramillo <alexunjm@gmail.com>
2020-06-27 17:02:20 -0500

    default access routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) },
+  { path: 'error', loadChildren: () => import('./control/error/error.module').then(m => m.ErrorModule) },
+  { path: 'access-denied', loadChildren: () => import('./control/access-denied/access-denied.module').then(m => m.AccessDeniedModule) },
+  { path: 'not-found', loadChildren: () => import('./control/not-found/not-found.module').then(m => m.NotFoundModule) },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
