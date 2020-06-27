a5ee0093b9248348e8e4609cc5ff98f8f57f16b7
alexanderjaramillo <alexunjm@gmail.com>
2020-06-27 17:18:02 -0500

    default layout (no login user)

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
+import { DefaultLayoutModule } from './default-layout/default-layout.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    LayoutRoutingModule,
+    DefaultLayoutModule
   ]
 })
 export class LayoutModule { }
