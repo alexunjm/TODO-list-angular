4be3e25f57928f89ea6ad368aa21bc1f3d3c500f
alexanderjaramillo <alexunjm@gmail.com>
2020-06-28 04:49:53 -0500

    basic logic of todolist

---
+++ src/app/shared/business/index.ts
@@ -0,0 +1,2 @@
+export { Item, BasicItem, MongoItem, SortableItem, MarkableItem, ItemDeadline } from './todo/todo';
+export * from './abstract-factory';
---
+++ src/app/shared/business/todo/todo.ts
@@ -0,0 +1,61 @@
+export interface BasicItem {
+  getName(): string;
+  getDate(): Date;
+}
+
+export interface MongoItem {
+  getId(): string;
+}
+
+export interface SortableItem {
+  getPriority(): number;
+}
+
+export interface MarkableItem {
+  isCompleted(): boolean;
+}
+
+export interface ItemDeadline {
+  isDueSoon(): boolean;
+  isOverdue(): boolean;
+}
+
+export interface Item extends BasicItem, MongoItem, SortableItem, MarkableItem, ItemDeadline {}
+
+export class ToDoItem implements Item {
+  id: string;
+  name: string;
+  priority: number;
+  completed: boolean;
+  date: Date;
+
+  constructor(data) {
+    this.id = data._id;
+    this.name = data.name;
+    this.priority = data.priority;
+    this.completed = data.completed;
+    this.date = data.date;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+  getDate(): Date {
+    return this.date;
+  }
+  getId(): string {
+    return this.id;
+  }
+  getPriority(): number {
+    return this.priority;
+  }
+  isCompleted(): boolean {
+    return this.completed;
+  }
+  isDueSoon(): boolean {
+    return (Date.now() - this.date.getTime()) < (1000 /* ms */ * 60 /* seg */ * 60/* min */);
+  }
+  isOverdue(): boolean {
+    return Date.now() > this.date.getTime();
+  }
+}
